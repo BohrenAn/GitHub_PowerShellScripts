57d5123e7239ddb48d4e28bf54500e5d729edd3c
Andres Bohren <a.bohren@icewolf.ch>
2022-04-06 17:51:09 +0200

    Azure Demos

--- Azure/Demo03-SQLDB-ARM.ps1
+++ Azure/Demo03-SQLDB-ARM.ps1
@@ -14,24 +14,44 @@ Connect-AzAccount
 Set-AzContext [SubscriptionID/SubscriptionName]
 
 ###############################################################################
-# Create Resource Group
-# https://docs.microsoft.com/en-us/powershell/module/az.resources/new-azresourcegroup?view=azps-7.4.0
+# New-AzDeployment (Create ResourceGroup)
+# https://docs.microsoft.com/en-us/powershell/module/az.resources/new-azdeployment?view=azps-7.4.0
+###############################################################################
+$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Template.json"
+$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Parameters.json"
+New-AzDeployment -TemplateFile $TemplateFile -TemplateParameterFile $ParameterFile -Location "westeurope"
+
+###############################################################################
+# New-AzResourceGroupDeployment (Create SQL Server / Database / Firewall Rules)
+# https://docs.microsoft.com/en-us/powershell/module/az.resources/new-azresourcegroupdeployment?view=azps-7.4.0
+###############################################################################
+
+$Securestring = ConvertTo-SecureString "SloppyJoe!" -AsPlainText -Force
+$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Template.json"
+$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Parameters.json"
+$ResourceGroup = "RG_Demo03"
+New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile $TemplateFile -TemplateParameterFile $ParameterFile -administratorLoginPassword $Securestring
+
+###############################################################################
+# Remove-AzResourceGroup
+# https://docs.microsoft.com/en-us/powershell/module/az.resources/remove-azresourcegroup?view=azps-7.4.0
 ###############################################################################
 $ResourceGroup = "RG_Demo03"
-$Location = "westeurope"
-New-AzResourceGroup -Name $ResourceGroup -Location $Location
+Remove-AzResourceGroup -Name $ResourceGroup -Force
+
 
 ###############################################################################
-# Create Resource Group
-# https://docs.microsoft.com/en-us/powershell/module/az.resources/new-azresourcegroup?view=azps-7.4.0
+# Deployment using Azure CLI
 ###############################################################################
-$projectName = Read-Host -Prompt "Enter the same project name"
-$templateFile = Read-Host -Prompt "Enter the template file path and file name"
-$resourceGroupName = "${projectName}rg"
+az login
 
-New-AzResourceGroupDeployment `
-  -Name DeployLocalTemplate `
-  -ResourceGroupName $resourceGroupName `
-  -TemplateFile $templateFile `
-  -projectName $projectName `
-  -verbose
+$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Template.json"
+$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Parameters.json"
+az deployment group create --template-uri $TemplateFile --parameters $ParameterFile --location "westeurope"
+
+
+$Securestring = ConvertTo-SecureString "SloppyJoe!" -AsPlainText -Force
+$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Template.json"
+$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Parameters.json"
+$ResourceGroup = "RG_Demo03"
+az deployment group create --template-uri $TemplateFile --parameters $ParameterFile --location "westeurope"
