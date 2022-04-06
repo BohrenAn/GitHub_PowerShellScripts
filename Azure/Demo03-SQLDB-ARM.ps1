26c76557c890546c9851b081081ef730599ca47f
Andres Bohren <a.bohren@icewolf.ch>
2022-04-06 22:11:35 +0200

    Azure Demos

--- Azure/Demo03-SQLDB-ARM.ps1
+++ Azure/Demo03-SQLDB-ARM.ps1
@@ -1,5 +1,6 @@
 ###############################################################################
-# SQLDB-Az.ps1 - Create SQL Server / Firewall Rule / SQL Database with AZ.* Powershell Modules
+# Demo03-SQLDB-ARM.ps1 
+# Create SQL Server / Firewall Rule / SQL Database with ARM Template (JSON)
 # 05.04.2022 - Initial Version - Andres Bohren 
 ###############################################################################
 
@@ -17,8 +18,8 @@ Set-AzContext [SubscriptionID/SubscriptionName]
 # New-AzDeployment (Create ResourceGroup)
 # https://docs.microsoft.com/en-us/powershell/module/az.resources/new-azdeployment?view=azps-7.4.0
 ###############################################################################
-$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Template.json"
-$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Parameters.json"
+$TemplateFile = "https://raw.githubusercontent.com/BohrenAn/GitHub_PowerShellScripts/main/Azure/Demo03-ResourceGroup-Template.json"
+$ParameterFile = "https://raw.githubusercontent.com/BohrenAn/GitHub_PowerShellScripts/main/Azure/Demo03-ResourceGroup-Parameters.json"
 New-AzDeployment -TemplateFile $TemplateFile -TemplateParameterFile $ParameterFile -Location "westeurope"
 
 ###############################################################################
@@ -27,8 +28,8 @@ New-AzDeployment -TemplateFile $TemplateFile -TemplateParameterFile $ParameterFi
 ###############################################################################
 
 $Securestring = ConvertTo-SecureString "SloppyJoe!" -AsPlainText -Force
-$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Template.json"
-$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Parameters.json"
+$TemplateFile = "https://github.com/BohrenAn/GitHub_PowerShellScripts/blob/main/Azure/Demo03-SQLDB-Template.json"
+$ParameterFile = "https://github.com/BohrenAn/GitHub_PowerShellScripts/blob/main/Azure/Demo03-SQLDB-Parameters.json"
 $ResourceGroup = "RG_Demo03"
 New-AzResourceGroupDeployment -ResourceGroupName $ResourceGroup -TemplateFile $TemplateFile -TemplateParameterFile $ParameterFile -administratorLoginPassword $Securestring
 
@@ -45,13 +46,19 @@ Remove-AzResourceGroup -Name $ResourceGroup -Force
 ###############################################################################
 az login
 
-$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Template.json"
-$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-ResourceGroup-Parameters.json"
-az deployment group create --template-uri $TemplateFile --parameters $ParameterFile --location "westeurope"
+$TemplateFile = "https://raw.githubusercontent.com/BohrenAn/GitHub_PowerShellScripts/main/Azure/Demo03-ResourceGroup-Template.json"
+$ParameterFile = "https://raw.githubusercontent.com/BohrenAn/GitHub_PowerShellScripts/main/Azure/Demo03-ResourceGroup-Parameters.json"
+az deployment sub create --template-uri $TemplateFile --parameters $ParameterFile --location "westeurope"
 
 
 $Securestring = ConvertTo-SecureString "SloppyJoe!" -AsPlainText -Force
-$TemplateFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Template.json"
-$ParameterFile = "C:\GIT_WorkingDir\GitHub_PowerShellScripts\Azure\Demo03-SQLDB-Parameters.json"
+$TemplateFile = "https://raw.githubusercontent.com/BohrenAn/GitHub_PowerShellScripts/main/Azure/Demo03-SQLDB-Template.json"
+$ParameterFile = "https://raw.githubusercontent.com/BohrenAn/GitHub_PowerShellScripts/main/Azure/Demo03-SQLDB-Parameters.json"
 $ResourceGroup = "RG_Demo03"
-az deployment group create --template-uri $TemplateFile --parameters $ParameterFile --location "westeurope"
+az deployment group create --resource-group $ResourceGroup --template-uri $TemplateFile --parameters $ParameterFile administratorLoginPassword=$Securestring
+
+###############################################################################
+#Cleanup
+###############################################################################
+$ResourceGroup = "RG_Demo03"
+az group delete --resource-group $ResourceGroup --yes
