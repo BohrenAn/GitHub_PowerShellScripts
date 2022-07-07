c2a49ddc6f7916d713b4c87ab3b37facf9b04c09
Andres Bohren <a.bohren@icewolf.ch>
2022-07-07 23:40:59 +0200

    Updates TeamsChannelEmailAddresses

---
+++ MicrosofTeams/Get-TeamsChannelEmailaddresses.ps1
@@ -0,0 +1,74 @@
+###############################################################################
+# Get Emailadresses from TeamsChannel
+# 25.04.2022 V0.1 Initial Version - Andres Bohren
+###############################################################################
+#Azure AD Role
+#-TeamsAdministrator
+#GraphPermissions
+#-Channel.ReadBasic.All
+#-ChannelSettings.Read.All
+#-Group.Read.All
+
+<#
+#AzureAD App Authentication (OAuth2)
+$APPID = "05be746b-7e2f-4e57-b99d-e793a7535d08"
+$CertificateThumbprint = "4F1C474F862679EC35650824F73903041E1E5742" #O365Powershell2.cer
+$TenantID = "icewolfch.onmicrosoft.com"
+Connect-MgGraph -TenantId $TenantID -AppId $APPID -CertificateThumbprint $CertificateThumbprint
+#>
+
+#Interactive Login
+Write-Host "Connect-MgGraph"
+Connect-MgGraph -Scopes Channel.ReadBasic.All, ChannelSettings.Read.All, ChannelSettings.ReadWrite.All
+
+
+#Get-Team
+Write-Host "Connect-MicrosoftTeams"
+$TeamsConnection = Connect-MicrosoftTeams 
+
+Write-Host "Getting Teams..."
+$TeamsArray = Get-Team
+$TeamsCount = $TeamsArray.count
+Write-Host "Teams found: $TeamsCount"
+
+#Create CSV File
+If ($PSScriptRoot -eq "")
+{
+	$Path = [System.Environment]::CurrentDirectory + "\TeamsChannelEmail.csv"
+} else {
+	$Path =$PSScriptRoot + "\TeamsChannelEmail.csv"
+}
+
+#Check if File Exist and delete if exists
+If (Test-Path -Path $Path)
+{
+	Remove-Item -Path $Path -Confirm:$false
+}
+
+#CreateCSVHeader
+Add-Content -Path $Path "TeamsObjectId;TeamsDisplayName;ChannelDisplayName;ChannelEmail"
+
+#Loop through Teams 
+$Int = 0
+Foreach ($Team in $TeamsArray)
+{
+	$Int = $Int + 1
+	$DisplayName = $Team.DisplayName
+	$GroupId = $Team.GroupId
+	Write-Host "Working on: $DisplayName [$Int/$TeamsCount]" -ForeGroundColor Green
+	
+	#Loop through Channels
+	$Channels = Get-MgTeamChannel -TeamId $GroupId
+	Foreach ($Channel in $Channels)
+	{
+		$ChannelDisplayName = $Channel.DisplayName
+		$ChannelEmail = $Channel.Email
+		If ($ChannelEmail -ne "")
+		{
+			#Email found
+			Write-Host "$ChannelDisplayName > $ChannelEmail"
+			#Add to CSV File
+			Add-Content -Path $Path "$GroupId;$DisplayName;$ChannelDisplayName;$ChannelEmail"
+		}
+	}
+}
