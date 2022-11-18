0ba2b6bf80987e1b4cc29bf32007e61df2271606
Andres Bohren <a.bohren@icewolf.ch>
2022-11-18 08:46:47 +0100

    Addet Process Check

--- Azure/Cleanup-AZModules.ps1
+++ Azure/Cleanup-AZModules.ps1
@@ -5,9 +5,23 @@
 # 25.04.2022 V0.1 - Initial Draft - Andres Bohren
 # 07.06.2022 V0.2 - Also uninstall AZ Module - Andres Bohren
 # 03.08.2022 V0.3 - Changed some commands to Get-InstalledModule
+# 18.11.2022 V0.4 - Addet Process check
 ###############################################################################
 #Script needs to Run as Administrator to uninstall/install PowerShell Modules
 #Requires -RunAsAdministrator
+
+#Check if VSCode or PowerShell is running
+[array]$process = Get-Process | Where-Object {$_.ProcessName -eq "powershell" -or $_.ProcessName -eq "pwsh" -or $_.ProcessName -eq "code"}
+#$process = Get-Process -Name code -ErrorAction SilentlyContinue
+If ($process.Count -gt 1)
+{
+	Write-Host "PowerShell or Visual Studio Code running? Please close it, otherwise the Modules sometimes can't be updated..." -ForegroundColor Red
+	
+	#Press any key to continue
+	Write-Host 'Press any key to continue...';
+	$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+	Write-Host "Checking Modules..."
+}
 
 #Remove loaded az.* Modules
 Remove-Module az.*
