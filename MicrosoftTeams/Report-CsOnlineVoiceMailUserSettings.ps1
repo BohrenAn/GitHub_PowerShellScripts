a1a19a5599687e226a3934557e5405d398d3ac09
Andres Bohren <a.bohren@icewolf.ch>
2023-06-14 10:55:07 +0200

    Updates 20230614

---
+++ MicrosoftTeams/Report-CsOnlineVoiceMailUserSettings.ps1
@@ -0,0 +1,30 @@
+###############################################################################
+# Report Teams VoiceMail Settings
+# 14.06.2023 - Initial Script - Andres Bohren
+# https://blog.icewolf.ch/archive/2022/06/25/microsoft-teams-voicemail-settings-for-users-and-admins/
+###############################################################################
+
+#Connect-MicrosoftTeams
+Connect-MicrosoftTeams
+
+#Get all Teams users
+$CSUsers = Get-CsOnlineUser -ResultSize 0
+
+Set-Content -Path .\TeamsVoiceMail.csv -Value "UPN;VoicemailEnabled;PromptLanguage"
+
+$INT = 0
+Foreach ($CSUser in $CSUsers)
+{
+	$INT = $INT +1
+	$UPN = $CSUser.UserPrincipalName
+	Write-Host "Working on: $UPN [$INT]"
+
+	#Get VoiceMail User
+	$VoiceMailSettings = Get-CsOnlineVoicemailUserSettings -Identity $UPN
+	$VoicemailEnabled = $VoiceMailSettings.VoicemailEnabled
+	$PromptLanguage = $VoiceMailSettings.PromptLanguage
+
+	#Save to VSV
+	Add-Content -Path .\TeamsVoiceMail.csv -Value "$UPN;$VoicemailEnabled;$PromptLanguage"
+
+}
