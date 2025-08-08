96256a0ba44240ce76fe3b47055850d8956d2189
Andres Bohren <a.bohren@icewolf.ch>
2025-08-08 21:13:42 +0200

    Update EXCommands

--- Exchange/Exchange_Commands.ps1
+++ Exchange/Exchange_Commands.ps1
@@ -7,3 +7,24 @@
 $cert = Get-ExchangeCertificate -Thumbprint <ThumbPrint>
 $tlscertificatename = "<i>$($cert.Issuer)<s>$($cert.Subject)"
 Set-ReceiveConnector "ICESRV06\Client Frontend ICESRV06" -TlsCertificateName $tlscertificatename
+
+
+###############################################################################
+# Check if running in Exchange Management Shell (EMS)
+###############################################################################
+$isEMS = [bool] (Get-Command -eq Ignore Get-ExCommand -ErrorAction SilentlyContinue)
+if ($isEMS)
+{ 
+    Write-Host "Using EMS"
+} else {
+    Write-Host "Using normal PS"
+}
+
+###############################################################################
+# Get mailbox statistics
+###############################################################################
+$MBXStat = Get-MailboxStatistics -Identity <MailboxIdentity> -ErrorAction SilentlyContinue
+#PowerShell Remoting
+$MBXStat.TotalItemSize.Value -replace '.*\(| bytes\).*|,' | % {'{0:N2}' -f ($_ / 1mb)}
+#EMS
+$MBXStat.TotalItemSize.Value.ToMB()
